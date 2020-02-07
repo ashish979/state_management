277269dec97440053e703f520fce4f33a2fa37ac
Ashish Agrawal <ashishagrawal979@gmail.com>
2020-02-07 12:05:12 +0530

    modify list component

--- src/components/List.js
+++ src/components/List.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 import ListItem from './ListItem';
 import ListHeader from './ListHeader';
 import EmptyList from './EmptyList';
+import ListStore from "../flux/store/ListStore";
 
 class List extends Component {
-  getListOfItemIds = items => Object.keys(items)
+  constructor(props) {
+    super(props);
+    this.state.items = ListStore.getListItems();
+  }
+
+  componentDidMount() {
+    ListStore.addChangeListener(this.updateList.bind(this));
+  }
+
+  componentWillUnmount() {
+    ListStore.removeChangeListener(this.updateList);
+  }
+
+  updateList() {
+    this.setState({
+      items: ListStore.getListItems()
+    })
+  }
+
+  getListOfItemIds = items => Object.keys(items);
 
   getTotalNumberOfListItems = items => (
     this.getListOfItemIds(items).reduce((accumulator, itemId) => (
@@ -14,37 +34,31 @@ class List extends Component {
 
   createListItemElements(items) {
     let item;
-    const { removeListItem } = this.props;
 
     return (
       this
       .getListOfItemIds(items)
       .map(itemId => {
         item = items[itemId];
-        return (<ListItem item={item} removeListItem={removeListItem} key={item.id} />);
+        return (<ListItem item={item} key={item.id} />);
       })
       .reverse()
     );
   }
 
   render() {
-    const { items, removeAllListItems } = this.props;
+    const items = this.state.items;
     const listItemElements = this.createListItemElements(items);
 
     return (
       <div>
         <h3>
-
           <ListHeader
             totalNumberOfListItems={this.getTotalNumberOfListItems(items)}
-            removeAllListItems={removeAllListItems}
           />
-
         </h3>
         <ul>
-
           {listItemElements.length > 0 ? listItemElements : <EmptyList />}
-
         </ul>
       </div>
     );
